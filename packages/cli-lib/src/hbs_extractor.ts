6221d1102dfc387ef1bd5570bd91168605336163
Stanislav Dunajcan (Stan) <stanislav.dunajcan@simplabs.com>
2021-12-13 08:55:21 +0100

    implement: subexpression in hbs_extractor

--- packages/cli-lib/src/hbs_extractor.ts
+++ packages/cli-lib/src/hbs_extractor.ts
@@ -1,19 +1,26 @@
 import {transform} from 'ember-template-recast'
 
+function extractText(node: any, fileName: any, options: any) {
+  if (node.path.original === 'format-message') {
+    let message = node.params[0]?.original
+    let desc = node.params[1]?.original
+    let id = options.overrideIdFn(undefined, message, desc, fileName)
+    options.onMsgExtracted(undefined, {
+      id: id,
+      defaultMessage: message,
+      description: desc,
+    })
+  }
+}
+
 export function parseFile(source: string, fileName: string, options: any) {
   let visitor = function () {
     return {
       MustacheStatement(node: any) {
-        if (node.path.original === 'format-message') {
-          let message = node.params[0]?.original
-          let desc = node.params[1]?.original
-          let id = options.overrideIdFn(undefined, message, desc, fileName)
-          options.onMsgExtracted(undefined, {
-            id: id,
-            defaultMessage: message,
-            description: desc,
-          })
-        }
+        extractText(node, fileName, options)
+      },
+      SubExpression(node: any) {
+        extractText(node, fileName, options)
       },
     }
   }
