472dd48d8cc3d91f8d8ab398edbb48f0fa5eb95f
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2022-07-27 23:23:29 +0200

    feat: the same messages inside nested html should have equal ids

--- packages/cli/src/hbs_extractor.ts
+++ packages/cli/src/hbs_extractor.ts
@@ -4,8 +4,8 @@ function extractText(node: any, fileName: any, options: any) {
   if (node.path.original === 'format-message') {
     let message = node.params[0]?.original
     let desc = node.params[1]?.original
-    let id = options.overrideIdFn(undefined, message, desc, fileName)
     let defaultMessage = message?.trim().replace(/\s+/gm, ' ')
+    let id = options.overrideIdFn(undefined, defaultMessage, desc, fileName)
     options.onMsgExtracted(undefined, {
       id: id,
       defaultMessage: defaultMessage,
