0a64c6dd757dd4f16fa0428c2ebc62c689587dfa
Stanislav Dunajcan (Stan) <stanislav.dunajcan@simplabs.com>
2022-08-15 13:03:39 +0200

    fix: the same description inside nested html should have equal ids

--- packages/cli/src/hbs_extractor.ts
+++ packages/cli/src/hbs_extractor.ts
@@ -3,10 +3,11 @@ import {transform} from 'ember-template-recast'
 function extractText(node: any, fileName: any, options: any) {
   if (node.path.original === 'format-message') {
     let message = node.params[0]?.original
-    let desc = node.params[1]?.original
+    let desc = node.params[1]?.original?.trim().replace(/\s+/gm, ' ')
     let defaultMessage = message?.trim().replace(/\s+/gm, ' ')
-    // overrideIdFn needs to receive defaultMessage with already trimmed whitespaces
+    // overrideIdFn needs to receive defaultMessage/description with already trimmed whitespaces
     // so the resulting id is the same if messages have the same content but have different indentation
+    // id has to be generated the same way in soxhub/ember-formatjs/replace-key-transform !!!!!!!!!!!!!!!!!!!!!!
     let id = options.overrideIdFn(undefined, defaultMessage, desc, fileName)
     options.onMsgExtracted(undefined, {
       id: id,
