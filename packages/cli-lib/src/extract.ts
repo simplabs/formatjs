8e2d90b427d9df2fbcb448e96b2bad8fd22229e4
Dave Laird <kiwiupover@gmail.com>
2023-08-31 00:01:58 -0700

    feature: add gts extractor

--- packages/cli-lib/src/extract.ts
+++ packages/cli-lib/src/extract.ts
@@ -147,6 +147,14 @@ async function processFile(
     debug('Processing %s using vue extractor', fn)
     const {parseFile} = await import('./vue_extractor')
     parseFile(source, fn, scriptParseFn)
+  } else if (fn.endsWith('.hbs')) {
+    debug('Processing %s using hbs extractor', fn)
+    const {parseFile} = await import('./hbs_extractor')
+    parseFile(source, fn, scriptParseFn)
+  } else if (fn.endsWith('.gts') || fn.endsWith('.gjs')) {
+    debug('Processing %s as gts/gjs file', fn)
+    const {parseFile} = await import('./gts_extractor')
+    parseFile(source, fn, opts)
   } else {
     debug('Processing %s using typescript extractor', fn)
     scriptParseFn(source)
@@ -208,7 +216,7 @@ export async function extract(
       const {id, description, defaultMessage} = message
       if (!id) {
         const error = new Error(
-          `[FormatJS CLI] Missing message id for message: 
+          `[FormatJS CLI] Missing message id for message:
 ${JSON.stringify(message, undefined, 2)}`
         )
         if (throws) {
